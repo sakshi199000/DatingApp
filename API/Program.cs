1624d6796b978211d3f3947c879d46bbccc5db07
sakshi199000 <sakshi.mehandiratta86@gmail.com>
2024-09-18 12:50:02 +0800

    EndOfSection3

--- API/Program.cs
+++ API/Program.cs
@@ -7,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatingAppDb>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));  
-
+builder.Services.AddCors();
 var app = builder.Build();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
